8a65bbfd23dc598e632863ec88b45e1c794bc031
Maxim <maxkolobov2004.09v@gmail.com>
2024-01-14 19:55:22 +0500

    test func update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 from config import TEST
-from src.utils import open_, executed_satatus, date_sorted, time_corrected
+from src.utils import open_, executed_satatus, date_sorted, time_corrected, change_numbers
 
 
 def test_open_():
@@ -26,3 +26,8 @@ def test_time_corrected():
     assert func == [{'date': '26.08.2019'},
                              {'date': '14.06.2019'},
                              {'date': '30.10.2019'}]
+
+def test_change_numbers():
+    assert change_numbers("Maestro 1596837868705199") == "Maestro 1596 83** **** 5199"
+    assert change_numbers("Счет 64686473678894779589") == "Счет **9589"
+
