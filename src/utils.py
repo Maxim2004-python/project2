e2b13ae85b330f3ad3759f5d370173ab221a5e69
Maxim <maxkolobov2004.09v@gmail.com>
2024-01-14 19:42:40 +0500

    func update

--- src/utils.py
+++ src/utils.py
@@ -19,15 +19,37 @@ def executed_satatus(data):
     return executed_operations
 
 
-
-
 def date_sorted(data):
     return sorted(data, key=lambda dictionary: dictionary['date'], reverse=True)[:5]
 
 def time_corrected(data):
+    new_list = []
     for time in data:
         wrong_time = date.strptime(time['date'], '%Y-%m-%dT%H:%M:%S.%f')
-        time['date'] = wrong_time.strftime('%d.%m.%y')
+        time['date'] = wrong_time.strftime('%d.%m.%Y')
+        new_list.append(time)
+    return(new_list)
+
+
+def change_numbers(key_card_info):
+    new_list = []
+    split_card_info = key_card_info.split()
+    for item in split_card_info:
+        if 'Счет' in key_card_info and item.isdigit():
+            new_list.append(f'{item[-4:]}')
+        elif 'Счет' not in key_card_info  and item.isdigit():
+            new_list.append(f'{item[:4]} {item[4:6]} **** {item[-4:]}')
+        else:
+            new_list.append(item)
+    return ' '.join(new_list)
+
+
+
+
+
+
+
+
 
 
 data_load = open_(DATA)
@@ -37,6 +59,8 @@ executed_data = executed_satatus(data_load)
 sorted_data = date_sorted(executed_data)
 
 changed_data = time_corrected(sorted_data)
+
+new_numebrs = change_numbers(changed_data)
 
 print(sorted_data)
 
