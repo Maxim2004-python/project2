62404e2180d574595aa3d6b51b333b402cdea128
Maxim <maxkolobov2004.09v@gmail.com>
2024-01-14 17:14:14 +0500

    test func update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 from config import TEST
-from src.utils import open_, executed_satatus
+from src.utils import open_, executed_satatus, date_sorted
 
 
 def test_open_():
@@ -9,4 +9,12 @@ def test_executed_satatus():
     assert executed_satatus([{'state': 'EXECUTED'},
                              {'state': 'WORD'},
                              {}]) == [{'state': 'EXECUTED'}]
+
+def test_date_sorted():
+    assert date_sorted([{'date': '2019-08-26T10:50:58.294041'},
+                             {'date': '2019-06-14T19:37:49.044089'},
+                             {'date': '2019-10-30T01:49:52.939296'}]) == [{'date': '2019-10-30T01:49:52.939296'},
+                                                                          {'date': '2019-08-26T10:50:58.294041'},
+                                                                          {'date': '2019-06-14T19:37:49.044089'}
+                                                                          ]
 
