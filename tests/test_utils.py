c5dcb4eb789af0f91ca65dc0a159c66e20d9859b
Maxim <maxkolobov2004.09v@gmail.com>
2024-01-14 17:48:17 +0500

    test func update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 from config import TEST
-from src.utils import open_, executed_satatus, date_sorted
+from src.utils import open_, executed_satatus, date_sorted, time_corrected
 
 
 def test_open_():
@@ -18,3 +18,11 @@ def test_date_sorted():
                                                                           {'date': '2019-06-14T19:37:49.044089'}
                                                                           ]
 
+def test_time_corrected():
+    func = [{'date': '2019-08-26T10:50:58.294041'},
+                             {'date': '2019-06-14T19:37:49.044089'},
+                             {'date': '2019-10-30T01:49:52.939296'}]
+    time_corrected(func)
+    assert func == [{'date': '26.08.2019'},
+                             {'date': '14.06.2019'},
+                             {'date': '30.10.2019'}]
