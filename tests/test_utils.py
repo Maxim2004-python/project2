08e252cecda3faa19f91a41103eb65765da7803b
Maxim <maxkolobov2004.09v@gmail.com>
2024-01-14 16:55:44 +0500

    test func update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,12 @@
 from config import TEST
-from src.utils import open_
+from src.utils import open_, executed_satatus
 
 
 def test_open_():
     assert open_(TEST) == [1, 2, 3]
+
+def test_executed_satatus():
+    assert executed_satatus([{'state': 'EXECUTED'},
+                             {'state': 'WORD'},
+                             {}]) == [{'state': 'EXECUTED'}]
+
